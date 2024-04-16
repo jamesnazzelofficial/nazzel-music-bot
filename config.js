7e47f50609d58f430c9d41343c7370c10b6da1e1
jamesnazzelofficial <148680292+jamesnazzelofficial@users.noreply.github.com>
2024-04-16 21:49:53 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    token: 'MTIyNzE4MjM3MDk0NDE5MjUxMw.GWm7pE.EjC38Xm55J1pwZiX9dA_PWOrx8pvz8R1xBW90w',
+    token: '',
     ownerId: '773412782514503740',
     mongoURI: 'mongodb+srv://jamesnazzeldevelopment:jnazzeldevelopment737@cluster0.z994hzc.mongodb.net/',
     defaultPrefix: '!',
